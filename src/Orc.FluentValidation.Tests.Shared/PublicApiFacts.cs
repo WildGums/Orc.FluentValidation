d8620c29dc641880741bfd37632bf0968d531242
Geert van Horrik <geert@catenalogic.com>
2018-03-27 16:30:16 +0200

    Migrate to new project system

---
+++ src/Orc.FluentValidation.Tests.Shared/PublicApiFacts.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PublicApiFacts.cs" company="WildGums">
+//   Copyright (c) 2008 - 2017 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.FluentValidation.Tests
+{
+    using ApiApprover;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PublicApiFacts
+    {
+        [Test]
+        public void Orc_FluentValidation_HasNoBreakingChanges()
+        {
+            var assembly = typeof(FluentValidatorProvider).Assembly;
+
+            PublicApiApprover.ApprovePublicApi(assembly);
+        }
+    }
+}
---
+++ src/Orc.FluentValidation/Attributes/ValidatorDescriptionAttribute.cs
@@ -0,0 +1,52 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ValidatorDescriptionAttribute.cs" company="WildGums">
+//   Copyright (c) 2008 - 2017 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.FluentValidation
+{
+    using System;
+    using Catel.Data;
+
+    /// <summary>
+    /// The validator description attribute.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class ValidatorDescriptionAttribute : Attribute
+    {
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ValidatorDescriptionAttribute" /> class.
+        /// </summary>
+        /// <param name="tag">The validation tag.</param>
+        /// <param name="validationResultType">The validation result type.</param>
+        /// <param name="validationType">The validation type.</param>
+        public ValidatorDescriptionAttribute(string tag, ValidationResultType validationResultType = ValidationResultType.Error,
+            ValidationType validationType = ValidationType.Field)
+        {
+            Tag = tag;
+            ValidationResultType = validationResultType;
+            ValidationType = validationType;
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets the validation tag.
+        /// </summary>
+        public string Tag { get; private set; }
+
+        /// <summary>
+        /// Gets the validation result type.
+        /// </summary>
+        public ValidationResultType ValidationResultType { get; private set; }
+
+        /// <summary>
+        /// Gets the validation type.
+        /// </summary>
+        public ValidationType ValidationType { get; private set; }
+        #endregion
+    }
+}
