d0f0e20b0219d8c247d5899ff72a033d67c1babd
Geert van Horrik <geert@catenalogic.com>
2023-01-12 17:42:08 +0100

    Fix analyzers

--- src/Orc.FluentValidation.Tests/FluentValidatorProviderFacts.cs
+++ src/Orc.FluentValidation.Tests/FluentValidatorProviderFacts.cs
@@ -1,10 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="FluentValidatorProviderFacts.cs" company="Catel development team">
-//   Copyright (c) 2008 - 2015 Catel development team. All rights reserved.
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
-
-namespace Catel.Test.Extensions.FluentValidation
+﻿namespace Catel.Test.Extensions.FluentValidation
 {
     using Catel.Data;
     using Catel.IoC;
